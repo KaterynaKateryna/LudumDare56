3f75f4726b0996ffa9ed4f5f161e501e0fd73eee
KaterynaKateryna <tok.ar.tok@gmail.com>
2024-10-05 14:34:23 +0200

    animate background

---
+++ src/scenes/background.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+@onready var color_rect = get_node("ColorRect")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var tween = get_tree().create_tween().set_loops()
+	var time = 5
+	tween.tween_property(color_rect, "modulate", Color("fad9c4"), time)
+	tween.tween_property(color_rect, "modulate", Color("f2e0a1"), time)
+	tween.tween_property(color_rect, "modulate", Color("a7f7b6"), time)
+	tween.tween_property(color_rect, "modulate", Color("83f8e9"), time)
+	#todo choose nicer colors
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
